a34d5db32bf8ea46c140792ec01909d376c3941b
hungmi <hungmihungmi@gmail.com>
2017-11-18 23:48:00 +0800

    basic sidebar and navbar are ready

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,16 @@
 class Admin::CustomersController < AdminController
-	before_action :find_customer
+	before_action :find_customer, :set_nav_title
 
 	def index
+		@customers = Admin::Customer.all
 	end
 
 	private
 	def find_customer
 		@customer = Admin::Customer.find(params[:id]) if params[:id]
+	end
+
+	def set_nav_title
+		@nav_title = "客戶管理"
 	end
 end
